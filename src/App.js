08ba108fd609a84b03e3a6def51b3354cb5af996
hye-ilee <harriet@kaist.ac.kr>
2025-01-11 15:28:37 +0900

    why no markers

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ const App = () => {
   const [currentStyleIndex, setCurrentStyleIndex] = useState(0);
   const [isLandingVisible, setIsLandingVisible] = useState(true);
   const [activeCategories, setActiveCategories] = useState(['all']);
+  const [markers, setMarkers] = useState([]);
 
   useEffect(() => {
     if (map.current) return; //이미 초기화된 경우 방지
@@ -42,12 +43,34 @@ const App = () => {
       zoom: 1.7,
       projection: projs[currentProjectionIndex],
     });
-  }, []); //초기화 시 한 번만 실행
+
+    map.current.on('load', () => {
+      console.log('Map has loaded:', map.current);
+      const marker1 = new mapboxgl.Marker()
+        .setLngLat([127.359245, 36.373794]) // 마커 좌표
+        .addTo(map.current); // 맵에 추가
+      console.log('Marker1 added:', marker1);
+    });
+  }, []);
 
   useEffect(() => {
     if (!map.current) return;
     map.current.setStyle(styles[currentStyleIndex]);
     map.current.setProjection(projs[currentProjectionIndex]);
+    const features = fetchCategoryData('all');
+    console.log('features:', features);
+    // const loadAllMarkers = async () => {
+    //   const features = await fetchCategoryData('all');
+    //   addMarkersToMap(features, categories.find((cat) => cat.id === 'all').color);
+    // };
+    // loadAllMarkers();
+    // map.current.on('load', () => {
+    //   console.log('Map has loaded:', map.current);
+    //   const marker1 = new mapboxgl.Marker()
+    //     .setLngLat([127.359245, 36.373794]) // 마커 좌표
+    //     .addTo(map.current); // 맵에 추가
+    //   console.log('Marker1 added:', marker1);
+    // });
   }, [currentProjectionIndex, currentStyleIndex]);
 
   const handleLandingClick = (e) => {
@@ -55,28 +78,66 @@ const App = () => {
     setIsLandingVisible(false);
   };
 
-  const handleCatClick = (id) => {
-    if (id === 'all') {
-      // "All" 버튼 클릭 시 다른 버튼 해제하고 "All"만 활성화
-      setActiveCategories(['all']);
+  const fetchCategoryData = async (category) => {
+    const limit = 20;
+    var url = '';
+    if(category == 'all') {
+      console.log('fetching all data');
+      const total = limit * (categories.length-1);
+      url = `http://172.10.7.20:8000/api/news/news_geojson/?limit=${total}`;
     } else {
+      url = `http://172.10.7.20:8000/api/news/news_geojson/?category=${category}&limit=${limit}`;
+    }
+    try {
+      console.log('fetching data from:', url);
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      return data.features;
+    } catch (error) {
+      console.error('Error fetching category data:', error);
+      return [];
+    }
+  };
+
+  const addMarkersToMap = (features, color) => {
+    markers.forEach((marker) => marker.remove());// 이전 마커 제거
+    const newMarkers = features.map((feature) => {
+      // const [lng, lat] = [127.259839,37.166155];
+      const [lng, lat] = feature.geometry.coordinates;
+      console.log('$', lng, lat);
+      const marker = new mapboxgl.Marker({ color, scale: 1.2,})
+        .setLngLat([lng, lat])
+        .addTo(map.current);
+      return marker;
+    });
+    setMarkers(newMarkers);
+  };
+
+  const handleCatClick = async (id) => {
+    if (id === 'all') {
+      setActiveCategories(['all']);
+      setMarkers([]);
+    } else {
+      console.log('why no marker');
       // 일반 버튼 클릭 시 "All" 버튼 비활성화
       setActiveCategories((prev) => {
         const updated = prev.includes(id)
           ? prev.filter((cat) => cat !== id) // 이미 활성화된 경우 비활성화
           : [...prev.filter((cat) => cat !== 'all'), id];
-        return updated.length === 0 ? ['all'] : updated; // 아무것도 선택되지 않으면 "All" 활성화
+        return updated;
       });
     }
+    const features = await fetchCategoryData(id);
+    console.log('features:', features);
+    addMarkersToMap(features, categories.find((cat) => cat.id === id).color);
   };
 
   return (
     <div>
       {isLandingVisible && (
-        <div
-          className={`landing ${!isLandingVisible ? 'hidden' : ''}`}
-          onClick={handleLandingClick}
-        >
+        <div className={`landing ${!isLandingVisible ? 'hidden' : ''}`}
+          onClick={handleLandingClick} >
           <h1>
           <span>Global news at a Glance,</span> <br />
           <span style={{ fontSize: '120px', fontWeight: 'bold'}}>Globance</span>
